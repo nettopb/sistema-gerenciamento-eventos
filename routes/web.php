76802fdfaa0c04acfd5eb5e56b33e5a29e7385c4
nettopb <160946325+nettopb@users.noreply.github.com>
2026-08-15 17:02:24 -0300

    r

--- routes/web.php
+++ routes/web.php
@@ -7,9 +7,11 @@ $router->get('/login', 'AuthController@login');
 $router->get('/eventos', 'EventoController@index');
 
 $router->get('/eventos/novo', 'EventoController@create');
+
 $router->post('/eventos/salvar', 'EventoController@store');
 
 $router->get('/eventos/editar', 'EventoController@edit');
+
 $router->post('/eventos/atualizar', 'EventoController@update');
 
 $router->post('/eventos/excluir', 'EventoController@delete');
